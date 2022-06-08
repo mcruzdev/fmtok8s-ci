c9ee1adadc659b4f359875c95e97dd4f9e4141ea
mcruzdev <matheus.lper@magnasistemas.com.br>
2022-06-08 00:10:41 -0300

    chore: add semver

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const core = require('@actions/core')
 const github = require('@actions/github')
+const { prerelease } = require('semver')
 
 var regex = new RegExp('(0|[1-9]\d*)+\.(0|[1-9]\d*)+\.(0|[1-9]\d*)+(-(([a-z-][\da-z-]+|[\da-z-]+[a-z-][\da-z-]*|0|[1-9]\d*)(\.([a-z-][\da-z-]+|[\da-z-]+[a-z-][\da-z-]*|0|[1-9]\d*))*))?(\\+([\da-z-]+(\.[\da-z-]+)*))?$')
 
-const tagPrefix = 'refs/tags/v'
+const tagsPrefix = 'refs/tags/v'
+const headsPrefix = 'refs/heads/'
 const tagStart = 10
+const branchStart = 10
+const defaultBranch = core.getInput('default-branch')
 
 function validateRef() {
     const { ref } = github.context
-    if (!ref.startsWith(tagPrefix)) {
+    if (!ref.startsWith(tagsPrefix)) {
         core.setFailed('[validateRef]: This action accepts only tag')
     }
 }
@@ -23,12 +27,48 @@ function validateTag() {
     }
 }
 
+function validateBranch() {
+    const { ref } = github.context
+    const branchName = ref.substring(branchStart)
+
+    console.log('[validateBranch]: branch ', branchName)
+
+    if (branchname !== defaultBranch) {
+        core.setFailed('[validateBranch]: This action runs just on default branch')
+    }
+}
+
+async function getLatestTag() {
+    const token = core.getInput('github_token')
+    const oktokit = github.getOctokit(token)
+
+    const tags = await oktokit.rest.repos.listTags({
+        repo: process.env.GITHUB_REPOSITORY,
+        per_page: 100,
+        owner: github.context.repo.owner
+    })
+
+    return tags.find((tag) => !prerelease(tag.name)) || {
+        name: "0.0.0",
+        commit: {
+            sha: "HEAD",
+        },
+    }
+
+}
+
 async function main() {
-    const ctx = JSON.stringify(github.context)
-    console.log('ctx: ')
-    console.log(ctx)
-    validateRef()
-    validateTag()
+
+    if (github.context.ref.startsWith(headsPrefix)) {
+        validateBranch()
+        const latest = await getLatestTag()
+        console.log('[main]: Latest tag: ', latest)
+    }
+
+    if (github.context.ref.startsWith(tagsPrefix)) {
+        validateRef()
+        validateTag()
+    }
 }
 
 main()
