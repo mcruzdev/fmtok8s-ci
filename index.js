52537b88db03b39ac42f24a825685c2d622d8bb6
mcruzdev <matheus.lper@magnasistemas.com.br>
2022-06-07 22:37:19 -0300

    test: testing

--- index.js
+++ index.js
@@ -4,11 +4,7 @@ const github = require('@actions/github')
 var regex = new RegExp('(0|[1-9]\d*)+\.(0|[1-9]\d*)+\.(0|[1-9]\d*)+(-(([a-z-][\da-z-]+|[\da-z-]+[a-z-][\da-z-]*|0|[1-9]\d*)(\.([a-z-][\da-z-]+|[\da-z-]+[a-z-][\da-z-]*|0|[1-9]\d*))*))?(\\+([\da-z-]+(\.[\da-z-]+)*))?$')
 
 async function setup() {
-    const { ref } = github.context
-    core.info(`ref: ${ref}`)
-    if (!regex.exec(tag)) {
-        core.setFailed('[fmtok8s-ci-action]: Invalid semver')
-    }
+    core.setFailed('failing')
 }
 
 setup()
